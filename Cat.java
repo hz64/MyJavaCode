ce137f0114da1a799087f4b0e5af5219097e847a
ZeAy <108943831+hz64@users.noreply.github.com>
2023-12-04 20:50:45 +0800

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,12 @@
+//小白写代码 改了三次终于可以运行了
+//难搞
+public class Cat{
+
+    public static void main(String[] aegs){
+         System.out.println("*");
+         System.out.println("你好");
+         System.out.println("我是Java大森");
+         System.out.println("非常棒！");
+         System.out.println("Good");
+    }
+}
